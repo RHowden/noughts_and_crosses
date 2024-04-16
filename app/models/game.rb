a31d78c27b7d258b2ed8b9b1754fa3d49f24dc90
RHowden <rhowden1975@gmail.com>
2024-04-16 16:05:27 +0100

    feat: Implement move! and state methods in the Game model
    These methods allow the players to make moves and will tell us when the game is over

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,76 @@ class Game < ApplicationRecord
     attribute :board, default: -> { 9.times.collect { nil } }
     attribute :current_player, default: -> { ["X", "O"].sample }
 
+    def state
+        return :x_wins if victory_for("x")
+        return :y_wins if victory_for("y")
+        return :draw if board.none? { |a| a.nil? }
+        :in_play 
+    end
+
+    def state_label
+        if state == :draw
+            "Game is drawn"    
+        elsif state == :x_wins 
+            "X has won"
+        elsif state == :y_wins
+            "Y has won"
+        end
+    end
+
+    def move!(index)
+        return unless state == :in_play
+
+        board[index] = current_player
+
+        self.current_player = current_player == "x" ? "y" : "x"
+
+        save!
+    end
+      
+    private
+
+    # This method uses pattern matching. It compares the board array
+    # to all the possible victory conditions.
+    def victory_for(player)
+        case board
+        in [
+                ^player, ^player, ^player, 
+                _, _, _,
+                _, _, _] |
+            [
+                _, _, _, 
+                ^player, ^player, ^player, 
+                _, _, _] |
+            [ 
+                _, _, _, 
+                _, _, _, 
+                ^player, ^player, ^player]
+          true
+        in [
+                ^player, _, _, 
+                ^player, _, _, 
+                ^player, _, _] |
+            [
+                _, ^player, _,
+                _, ^player, _,
+                _, ^player, _] |
+            [ 
+                _, _, ^player, 
+                _, _, ^player, 
+                _, _, ^player]
+          true
+        in [
+                ^player, _, _, 
+                _, ^player, _, 
+                _, _, ^player] |
+            [
+                _, _, ^player,
+                _, ^player, _,
+                ^player, _, _] 
+            true
+        else
+            false
+        end
+    end
 end
